002e7b2aea349587acb4e15b7510ec171c6cf53d
Beston Nounrasi <bcreativer@gmail.com>
2016-08-15 10:21:05 +1000

    Added devise and user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
+  root 'home#index'
 
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
